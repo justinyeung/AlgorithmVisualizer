6bcecb89f910f6f9ba7fe616e280e482bc464761
Justin <justingcyeung@gmail.com>
2020-03-23 21:33:25 -0400

    continue working on dijkstra

--- src/Algorithms/DijkstrasAlgorithm.java
+++ src/Algorithms/DijkstrasAlgorithm.java
@@ -2,37 +2,72 @@ package Algorithms;
 
 import Model.CanvasCoords;
 import Model.Square;
+import Model.SquareType;
 
 public class DijkstrasAlgorithm extends Search{
 	
 	private Integer[][] distances;
-	private boolean[][] shortestPath;
 	private final int numVertices = numI * numJ;
-
+	
 	public DijkstrasAlgorithm(CanvasCoords root, Square[][] squareArray) {
 		super(root, squareArray);
 		
-		int count = 0;
-//		initialize distances array
+//		initialize distances array to be infinity for every value
 		this.distances = new Integer[numI][numJ];
 		for(int i = 0; i < numI; i++) {
 			for(int j = 0; j < numJ; j++) {
 				distances[i][j] = Integer.MAX_VALUE;
-				count++;
 			}
 		}
-		System.out.println(count);
 //		initialize shortest path boolean array
-		shortestPath = new boolean[numI][numJ];
+	}
+
+	private CanvasCoords nextMinDist() {
+//		initialize min value
+		int min = Integer.MAX_VALUE;
+		CanvasCoords minCoord = new CanvasCoords(-1, -1);
+		
+//		loop through entire grid (squareArray)
+		for(int i = 0; i < 63; i++) {
+			for(int j = 0; j < 25; j++) {
+				if(visitedArray[i][j] == false && distances[i][j] <= min) {
+					min = distances[i][j];
+					minCoord = new CanvasCoords(i*20, j*20);
+				}
+			}
+		}
+		return minCoord;
+	}
+	
+	@Override
+	public void search() {
+		
+		CanvasCoords current;
 		
 //		initialize root distance as 0
 		distances[root.getArrayX()][root.getArrayY()] = 0;
-
-	}
-
-	@Override
-	public CanvasCoords search() {
-		return null;
+		
+		for(int count = 0; count < numVertices - 1; count++) {
+//			use nextMinDist to find next shortest distance from current square
+//			returns CanvasCoords of new current square
+			current = nextMinDist();
+			
+//			check if current is destination
+			if(squareArray[current.getArrayX()][current.getArrayY()].getType() == SquareType.DESTINATION) {
+				System.out.println(current.getArrayX()+", "+current.getArrayY());
+				return;
+			}
+			
+//			mark current square in visitedArray
+			visitedArray[current.getArrayX()][current.getArrayY()] = true;
+//			asdfasdfasdfsd
+//			update distance values for adjacent squares (from node to root)
+			for(int v = 0; v < numVertices; v++) {
+//				update if not visited, 
+//				if distance from root to node is through v is less than distance of v
+				
+			}
+		}
 	}
 	
 }
